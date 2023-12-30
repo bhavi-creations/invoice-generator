1bfac1f18c9f09d710aa75d02226f04beff796f4
Uday Veduruvada <uday.veduruvada@thegisolutions.com>
2023-12-30 19:38:20 +0530

    db details with condition added

--- bhavidb.php
+++ bhavidb.php
@@ -1,12 +1,21 @@
 <?php
     $server='localhost';
-    $username='root';
-    $pass='';
-    $database='bhavi_invoice_db';
+    // Condition to check if the script is running locally or on a server
+    if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+        // Local environment details
+        $username = 'root';
+        $pass = '';
+        $database = 'bhavi_invoice_db';
+    } else {
+        // Server environment details
+        $username = 'cnpthbbs_invoice_user';
+        $pass = '%tNc6peV4-}w';
+        $database = 'cnpthbbs_invoice';
+    }
 
     $conn = mysqli_connect($server,$username,$pass,$database);
-if(!$conn){
-    echo "connection failed";
-}
+    if(!$conn){
+        echo "connection failed";
+    }
 
 ?>
--- index.php
+++ index.php
@@ -7,10 +7,19 @@ require_once('bhavidb.php');
 
 function getInvoiceId()
 {
-    $server = 'localhost';
-    $username = 'root';
-    $pass = '';
-    $database = 'bhavi_invoice_db';
+    $server='localhost';
+    // Condition to check if the script is running locally or on a server
+    if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
+        // Local environment details
+        $username = 'root';
+        $pass = '';
+        $database = 'bhavi_invoice_db';
+    } else {
+        // Server environment details
+        $username = 'cnpthbbs_invoice_user';
+        $pass = '%tNc6peV4-}w';
+        $database = 'cnpthbbs_invoice';
+    }
 
     $conn = mysqli_connect($server, $username, $pass, $database);
 
